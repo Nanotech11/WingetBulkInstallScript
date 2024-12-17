ac9588bce0f3c7539b20178ef0c3bc17be2489e6
Noah <>
2024-12-17 00:39:01 -0600

    removed --silent from each line to resolve some compatability issues (e.g. calibre) and added crystal disk mark and info

--- Winget_New_PC_Setup.bat
+++ Winget_New_PC_Setup.bat
@@ -1,85 +1,87 @@
-REM winget upgrade --all --include-unknown --silent --force
+REM winget upgrade --all --include-unknown
 
 
 @REM Languages, Runtime, Dev
-:: winget install EclipseAdoptium.Temurin.20.JDK --silent
-:: winget install Python.Python.3.12 --silent
-:: winget install Microsoft.DirectX --silent
-:: winget install CondaForge.Miniforge3 --silent
-:: winget install MSYS2.MSYS2 --silent
-:: winget install Cygwin.Cygwin --silent
+:: winget install EclipseAdoptium.Temurin.20.JDK
+:: winget install Python.Python.3.12
+:: winget install Microsoft.DirectX
+:: winget install CondaForge.Miniforge3
+:: winget install MSYS2.MSYS2
+:: winget install Cygwin.Cygwin
 
 @REM Browsers
-:: winget install Brave.Brave --silent
-:: winget install Mozilla.Firefox --silent
+:: winget install Brave.Brave
+:: winget install Mozilla.Firefox
 
 @REM General Util
-:: winget install 7zip.7zip --silent
-:: winget install Git.Git --silent
-:: winget install StefanSundin.Superf4 --silent
-:: winget install Flameshot.Flameshot --silent
-:: winget install voidtools.Everything --silent
-:: winget install schollz.croc --silent
-:: winget install AntibodySoftware.WizTree --silent
-:: winget install Microsoft.Sysinternals.ProcessMonitor --silent
-:: winget install Microsoft.Sysinternals.ProcessExplorer --silent
-:: winget install Microsoft.Sysinternals.Autoruns --silent
-:: winget install XP89DCGQ3K6VLD --accept-package-agreements --silent &@REM PowerToys
-:: winget install 9P8LTPGCBZXD --accept-package-agreements --silent &@REM WinToys
-:: winget install 9PF4KZ2VN4W9 --accept-package-agreements --silent &@REM TranslucentTB
-:: winget install RamenSoftware.Windhawk --silent
-:: winget install Microsoft.WindowsTerminal --silent
-:: winget install Microsoft.PowerShell --silent
-:: winget install Chocolatey.Chocolatey --silent
-:: winget install Klocman.BulkCrapUninstaller --silent
-:: winget install TechPowerUp.NVCleanstall --silent
+:: winget install 7zip.7zip
+:: winget install Git.Git
+:: winget install StefanSundin.Superf4
+:: winget install Flameshot.Flameshot
+:: winget install voidtools.Everything
+:: winget install schollz.croc
+:: winget install AntibodySoftware.WizTree
+:: winget install Microsoft.Sysinternals.ProcessMonitor
+:: winget install Microsoft.Sysinternals.ProcessExplorer
+:: winget install Microsoft.Sysinternals.Autoruns
+:: winget install XP89DCGQ3K6VLD --accept-package-agreements &@REM PowerToys
+:: winget install 9P8LTPGCBZXD --accept-package-agreements &@REM WinToys
+:: winget install 9PF4KZ2VN4W9 --accept-package-agreements &@REM TranslucentTB
+:: winget install RamenSoftware.Windhawk
+:: winget install Microsoft.WindowsTerminal
+:: winget install Microsoft.PowerShell
+:: winget install Chocolatey.Chocolatey
+:: winget install Klocman.BulkCrapUninstaller
+:: winget install TechPowerUp.NVCleanstall
 
 @REM Audio, Video, Image
-:: winget install GIMP.GIMP --silent
-:: winget install Inkscape.Inkscape --silent
-:: winget install dotPDNLLC.paintdotnet --silent
-:: winget install Audacity.Audacity --silent
-:: winget install OBSProject.OBSStudio --silent
-:: winget install HandBrake.HandBrake --silent
-:: winget install GuinpinSoft.MakeMKV --silent
-:: winget install PeterPawlowski.foobar2000 --silent
-:: winget install VideoLAN.VLC --silent
-:: winget install yt-dlp.yt-dlp --silent
-:: winget install Gyan.FFmpeg --silent
+:: winget install GIMP.GIMP
+:: winget install Inkscape.Inkscape
+:: winget install dotPDNLLC.paintdotnet
+:: winget install Audacity.Audacity
+:: winget install OBSProject.OBSStudio
+:: winget install HandBrake.HandBrake
+:: winget install GuinpinSoft.MakeMKV
+:: winget install PeterPawlowski.foobar2000
+:: winget install VideoLAN.VLC
+:: winget install yt-dlp.yt-dlp
+:: winget install Gyan.FFmpeg
 
 @REM Document Editing
-:: winget install QPDF.QPDF --silent
-:: winget install Microsoft.Office --silent
-:: winget install TheDocumentFoundation.LibreOffice --silent
-:: winget install Notepad++.Notepad++ --silent
-:: winget install Microsoft.VisualStudioCode --silent
-:: winget install Microsoft.VisualStudio.2022.Community --silent
+:: winget install QPDF.QPDF
+:: winget install Microsoft.Office
+:: winget install TheDocumentFoundation.LibreOffice
+:: winget install Notepad++.Notepad++
+:: winget install Microsoft.VisualStudioCode
+:: winget install Microsoft.VisualStudio.2022.Community
 
 @REM General Programs
-:: winget install qBittorrent.qBittorrent --silent
-:: winget install Oracle.VirtualBox --silent
-:: winget install Balena.Etcher --silent
-:: winget install Malwarebytes.Malwarebytes --silent
-:: winget install calibre.calibre --silent
-:: winget install Nvidia.Broadcast --silent
-:: winget install 9NF8H0H7WMLT --accept-package-agreements --silent &@REM NVIDIA Control Panel
-:: winget install Nvidia.GeForceExperience --silent
-:: winget install Discord.Discord --silent
-:: winget install Gajim.Gajim --silent
+:: winget install qBittorrent.qBittorrent
+:: winget install Oracle.VirtualBox
+:: winget install Balena.Etcher
+:: winget install Malwarebytes.Malwarebytes
+:: winget install calibre.calibre
+:: winget install Nvidia.Broadcast
+:: winget install 9NF8H0H7WMLT --accept-package-agreements &@REM NVIDIA Control Panel
+:: winget install Nvidia.GeForceExperience
+:: winget install Discord.Discord
+:: winget install Gajim.Gajim
 
 @REM Game Launchers
-:: winget install Valve.Steam --silent
-:: winget install Ubisoft.Connect --silent
-:: winget install EpicGames.EpicGamesLauncher --silent
-:: winget install ElectronicArts.EADesktop --silent
+:: winget install Valve.Steam
+:: winget install Ubisoft.Connect
+:: winget install EpicGames.EpicGamesLauncher
+:: winget install ElectronicArts.EADesktop
 
 @REM Benching, Overclocking
-:: winget install Guru3D.Afterburner --silent
-:: winget install CPUID.CPU-Z --silent
-:: winget install CPUID.HWMonitor --silent
-:: winget install TechPowerUp.GPU-Z --silent
-:: winget install REALiX.HWiNFO --silent
-:: winget install Unigine.HeavenBenchmark --silent
-:: winget install Unigine.ValleyBenchmark --silent
+:: winget install Guru3D.Afterburner
+:: winget install CPUID.CPU-Z
+:: winget install CPUID.HWMonitor
+:: winget install TechPowerUp.GPU-Z
+:: winget install REALiX.HWiNFO
+:: winget install CrystalDewWorld.CrystalDiskInfo
+:: winget install CrystalDewWorld.CrystalDiskMark
+:: winget install Unigine.HeavenBenchmark
+:: winget install Unigine.ValleyBenchmark
 
 pause
